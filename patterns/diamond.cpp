6fc65f00ed6316647908bed8a813b73b72ca337d
yash-7575 <yashbhagyawant70@gmail.com>
2025-12-25 13:58:42 +0530

    Done diamond pattern

--- patterns/diamond.cpp
+++ patterns/diamond.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 using namespace std;
 
-// Logic:
-// 1. First count total number of elements to print per line
-// 2. Then count total number of spaces and total number of stars
-// 3. Print spaces and stars according to order
 int main()
 {
     int n;
     cin>>n;
+    // For Upper Half
     for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n+i-1; j++)
+        {
+            if(j<=n-i)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<"\n";
+    }
+    
+    // For Lower Half
+    for(int i=n; i>=1; i--)
     {
         for(int j=1; j<=n+i-1; j++)
         {
@@ -26,3 +40,4 @@ int main()
     }
     return 0;
 }
+// 😊✨
