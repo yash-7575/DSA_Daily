8925baea3143d5b52f61f1dde5e31cd7eff1387f
yash-7575 <yashbhagyawant70@gmail.com>
2026-01-01 22:01:21 +0530

    recursion print 1 to n

---
+++ recursion/print_1_to_n.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void print_1_to_n(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+    print_1_to_n(n-1);
+    cout<<n<<" ";
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print_1_to_n(n);
+    return 0;
+}
