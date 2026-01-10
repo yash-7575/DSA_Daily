56743103176a55c40c17b622fe4a7979bda061d4
yash-7575 <yashbhagyawant70@gmail.com>
2026-01-10 15:17:17 +0530

    Done with vectors pass by val and pass by ref

---
+++ cpp stl/vector_by_ref.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int add(vector<int> &v)
+{
+    v[0]=v[0]+10;
+    return v[0];
+}
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    int first=add(v);
+    cout<<first<<endl;
+    cout<<v[0];
+    //Output : 
+    // 20
+    // 20
+    // bcz its passes ref
+    return 0;
+}
