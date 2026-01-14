4ebb82b3ddc6ed08365b1ff33fc96953706c2de3
yash-7575 <yashbhagyawant70@gmail.com>
2026-01-14 15:18:01 +0530

    sec_large_O(2n)
    max find kro then jab tak usse chota mil nahi jata tab tak find kro

---
+++ arrays/easy/sec_large_O2n.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int second_largest(vector<int> &arr, int n)
+{
+    int max=arr[0];
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    int sec_large=-1;
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>sec_large && arr[i]!=max)
+        {
+            sec_large=arr[i];
+        }
+    }
+    return sec_large;
+}
+
+int main()
+{
+    vector<int> arr = {1,3,2,4,1};
+    vector<int> arr1 = {1,1,1};
+    int sec_large=second_largest(arr,arr.size());
+    cout<<sec_large<<endl;
+    sec_large=second_largest(arr1,arr1.size());
+    cout<<sec_large;
+    return 0;
+}
